01787ae8fefcba8401736f68eeab08df87d8dbd2
CheaterCodes <cheater.codes@outlook.com>
2020-01-18 16:08:00 +0100

    Fixed flying ash_clay and ash_clay_brick

--- src/main/java/net/cheatercodes/ash_clay/block/AshClayBrickBlock.java
+++ src/main/java/net/cheatercodes/ash_clay/block/AshClayBrickBlock.java
@@ -2,7 +2,9 @@ package net.cheatercodes.ash_clay.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.datafixer.fix.ChunkPalettedStorageFix;
 import net.minecraft.entity.EntityContext;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
@@ -10,6 +12,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.WorldView;
 
 public class AshClayBrickBlock extends HorizontalFacingBlock {
     private static final VoxelShape X_AXIS_SHAPE = Block.createCuboidShape(4, 0, 6, 12, 3, 10);
@@ -34,5 +38,16 @@ public class AshClayBrickBlock extends HorizontalFacingBlock {
         if(state.get(FACING).getAxis() == Direction.Axis.X)
             return X_AXIS_SHAPE;
         else
-            return Z_AXIS_SHAPE;    }
+            return Z_AXIS_SHAPE;
+    }
+
+    @Override
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        return world.getBlockState(pos.down()).isSideSolidFullSquare(world, pos, Direction.UP);
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction facing, BlockState neighborState, IWorld world, BlockPos pos, BlockPos neighborPos) {
+        return facing == Direction.DOWN && !state.canPlaceAt(world, pos.down()) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, facing, neighborState, world, pos, neighborPos);
+    }
 }
